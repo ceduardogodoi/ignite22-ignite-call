c08eac316dd1a03e2d4cdb75e6a341f8ba8b4e61
ceduardogodoi <carloseduardoalvesgodoi@hotmail.com>
2023-07-08 09:36:39 -0300

    Bloqueando dias da semana

--- src/pages/api/users/[username]/availability.api.ts
+++ src/pages/api/users/[username]/availability.api.ts
@@ -1,6 +1,7 @@
 import { NextApiRequest, NextApiResponse } from 'next'
-import { prisma } from '../../../../lib/prisma'
 import dayjs from 'dayjs'
+
+import { prisma } from '../../../../lib/prisma'
 
 export default async function handle(
   request: NextApiRequest,
